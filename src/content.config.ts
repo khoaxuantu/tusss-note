224f55c58d5089b4acbf0c7f909f390b0f7a4a27
khoaxuantu <ngxktuzkai2000@gmail.com>
2026-04-14 00:43:53 +0700

    feat: migrate articles

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,46 @@
+import type { CollectionConfig } from "astro/content/config";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 
+const ArticleSchema = z.object({
+  title: z.string(),
+  date: z.coerce.date(),
+  author: z.string().optional(),
+  prev_article: z
+    .object({
+      path: z.string(),
+      title: z.string(),
+    })
+    .optional(),
+  next_article: z
+    .object({
+      path: z.string(),
+      title: z.string(),
+    })
+    .optional(),
+});
+
+const articleDir = ["dsa", "js", "oop", "system-design"] as const;
+const articleColl: Record<
+  `articles.${(typeof articleDir)[number]}`,
+  CollectionConfig<typeof ArticleSchema>
+> = {} as Record<string, CollectionConfig<typeof ArticleSchema>>;
+
+articleDir.forEach((dir) => {
+  const key = `articles.${dir}`;
+  articleColl[key] = defineCollection({
+    loader: glob({
+      base: `./src/articles/${dir}`,
+      pattern: [`**/*.{md,mdx}`, "!index.{md,mdx}"],
+      retainBody: false,
+    }),
+    schema: ArticleSchema,
+  });
+});
+
 const topicColl = defineCollection({
-  loader: glob({ base: "./src/articles", pattern: "*/index.{md,mdx}" }),
+  loader: glob({ base: "./src/articles", pattern: "*/index.{md,mdx}", retainBody: false }),
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
@@ -11,4 +48,4 @@ const topicColl = defineCollection({
   }),
 });
 
-export const collections = { topic: topicColl };
+export const collections = { topic: topicColl, ...articleColl };
